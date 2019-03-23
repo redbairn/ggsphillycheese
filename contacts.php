0453d082a44684a44fdcfe67a60cdf5ea7af9d87
Philip Godkin <philip.godkin@gmail.com>
2019-03-23 16:57:19 +0000

    Create contacts.php
    Create contacts page

---
+++ contacts.php
@@ -0,0 +1,16 @@
+<?php
+/************************************************************************************* 
+* Authors: Craig Bell, Philip Godkin, Ying Godkin, Kristan Johnson
+* Assignment: Web Design Project, National College of Ireland, 2019.
+* Date: 2019/03/23
+**************************************************************************************/
+//define page title
+$title = 'Contacts';
+$home = 'active';
+
+include ('includes/header.php'); 
+ ?>
+<h1>Contact Page</h1>
+
+
+<?php include ('includes/footer.php'); ?>
