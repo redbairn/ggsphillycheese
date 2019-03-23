e111e44509ad0e4b3129291aff8fd196b8e7b72d
Philip Godkin <philip.godkin@gmail.com>
2019-03-23 12:41:05 +0000

    tying to get stylesheet to work

--- index.php
+++ index.php
@@ -8,9 +8,17 @@
 $title = 'Homepage';
 include ('includes/header.php'); 
  ?>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Homepage Header</h1>
+	<p>Test</p>
+</body>
+</html>
 
-<h1>Homepage Header</h1>
-<p>Test</p>
 
 
 
