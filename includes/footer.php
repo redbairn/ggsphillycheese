a705b437333bcaf2b3fe2a3ebc45aecf026809d9
Craig <craigdanielbell@gmail.com>
2019-03-24 16:51:37 +0000

    Style changes to the footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,11 @@
 
 		
 		<footer>
-			<p class="footer_content">Created by: TeamB, Web Design @ NCI (2019).</p>
-			<p class="footer_content"><a href="contact.php">Contact Us</a></p>
+		<div class="footer_container">
+			<p class="footer_content_l">Follow Us</p>
+			<p class="footer_content_c">Created by: TeamB, Web Design @ NCI (2019).</p>
+			<p class="footer_content_r"><a href="contact.php">Contact Us</a></p>
+		</div>
 		</footer>
 	</div><!--End of Wrapper-->
 	</body>
