5d2edcda8e75988f0b73f282586c42e2ef6f6337
Craig Bell <craigdanielbell@gmail.com>
2019-04-05 22:40:44 +0100

    Navigation almost perfect in mobile
    Issue with the javascript but design is all set up.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-function myHandlebar() {
-  var x = document.getElementById("top_nav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+
+navBarToggle.addEventListener('click', function () {
+	mainNav.classList.toggle('active');
+});
+
 
 
 
