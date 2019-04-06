1d934ce888b6edd5df5a9a027396e3ca6ae501e5
Craig <craigdanielbell@gmail.com>
2019-04-07 00:11:53 +0100

    Making alternative navigation
    Making alternative navigation for responsiveness.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-
-navBarToggle.addEventListener('click', function () {
-	mainNav.classList.toggle('active');
-});
-
-
-
-
-
 // Contacts Page Jscript
 // Google Maps
 
